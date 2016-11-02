95138b9331b45a1422be5e35c5742d92d094497c
Scott Tilney <stilney@ucsd.edu>
2016-11-02 12:31:43 -0700

    added budget page with progress bar

--- routes/add.js
+++ routes/add.js
@@ -19,6 +19,9 @@ exports.addFriend = function(req, res) {
 
 var data = require("../data.json");
 
+
+
+
 exports.addExpense = function(req, res) {
 	var price = req.query.price;
 	var name = req.query.name;
@@ -60,7 +63,21 @@ exports.addExpense = function(req, res) {
 			"price": price
 		});
 	}
+
+
+	console.log("data.food.length: " + data.food.length);
+	var index;
+	var foodTotal = 0;
+	for (index = 0; index < data.food.length; ++index) {
+    console.log(data.food[index].name + " " + data.food[index].price );
+    foodTotal = foodTotal + data.food[index].price;
+}
+
 	
+       //when the document is finished loading, replace everything
+       //between the <a ...> </a> tags with the value of splitText
+   //document.getElementById("foodTotal").innerHTML="foodTotal";
+
 
   res.render('expenses', data);
   console.log("yay, addExpense just ran!");
